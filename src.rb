8752be4a4ac9a2250393279c6a5d56d513a328bc
BALLESTER HERNANDEZ <alu4371@cc407.(none)>
2013-10-08 15:01:20 +0100

    Multiplicacion en Ruby

--- src.rb
+++ src.rb
@@ -1,2 +1,50 @@
 #!/usr/local/rvm/rubies/ruby-2.0.0-p247/bin/ruby
 
+def multiplicacion ()
+  
+  #operandos
+  a=[[2, 10,3],[30, 40,3], [30, 40, 3]]
+  b=[[10, 2, 3],[3, 9, 5],[3, 9, 5]]
+  
+  #resultado
+  c=[[0, 0, 0],[0, 0, 0],[0, 0, 0]]
+  
+  i=0
+  j=0
+  k=0
+  
+  # a.size -->filas
+  # a[0].size 0-->columnas
+  
+  if a[0].size == b.size #numero columnas de A igual a filas de B 
+    until i== a[0].size do #columnas de A
+      until j== b.size do #filas de B
+        until k==b.size  do
+          c[i][j]+= a[i][k]*b[k][j]
+          k+=1;
+        end
+        k=0;
+        j+=1
+      end
+      j=0
+      i+=1
+    end
+    
+    i=0
+
+    until i==b.size do
+      print(c[i])
+      puts "\n"
+      i+=1
+    end
+   
+  
+  else
+  
+    puts ("Error de tamaño")
+  
+  end
+  
+end
+
+multiplicacion()
